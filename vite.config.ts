758e40522299399f738b0e973f11d4ccd4593431
Tobshub <tobanigeorge@gmail.com>
2023-11-02 07:23:15 +0100

    add: tsconfigpaths and new packages

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,13 @@ import { defineConfig } from "vite";
 import path from "node:path";
 import electron from "vite-plugin-electron/simple";
 import react from "@vitejs/plugin-react";
-import resolvePaths from "vite-tsconfig-paths";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    resolvePaths(),
+    tsconfigPaths(),
     electron({
       main: {
         // Shortcut of `build.lib.entry`.
