51544e507c02afd2f590b809545cc1ea8e889731
Tobshub <tobanigeorge@gmail.com>
2023-11-02 07:27:47 +0100

    add: welcome page

--- src/pages/welcome.tsx
+++ src/pages/welcome.tsx
@@ -1,7 +1,19 @@
+import "@/styles/welcome.css";
+import { Button } from "@mui/material";
+import LaunchIcon from "@mui/icons-material/Launch";
+import { Link } from "react-router-dom";
+
 export default function Welcome() {
   return (
-    <div>
+    <main className="welcome-page">
       <h1>Welcome to the TDB GUI</h1>
-    </div>
+      <p>TDB server administration made easy.</p>
+      <div>
+        <Link to="/new">
+          <Button color="success">New Connection</Button>
+        </Link>
+        <Button endIcon={<LaunchIcon />}>Read the Docs</Button>
+      </div>
+    </main>
   );
 }
