e973d361253861e37753505ca38be1baacc80487
Tobshub <tobanigeorge@gmail.com>
2023-11-02 17:57:07 +0100

    fix: throw error if connection is closed

--- src/utils/tdb-wrapper.ts
+++ src/utils/tdb-wrapper.ts
@@ -31,12 +31,19 @@ export class Tobsdb {
     data: object | object[] | undefined,
     where?: object | undefined
   ): Promise<{ status: number; message: string; data: object | object[] }> {
+    if (this.ws.readyState >= 2) {
+      throw new Error("Websocket connection has closed");
+    }
+
     const q = JSON.stringify({ action, table, data, where });
     this.ws.send(q);
-    return new Promise<any>((res) => {
+    return new Promise<any>((res, rej) => {
       this.ws.onmessage = (ev) => {
         const data = JSON.parse(ev.data);
         res(data);
+      };
+      this.ws.onclose = () => {
+        rej("Websocket connection has closed");
       };
     });
   }
